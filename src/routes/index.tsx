f602d83212c8a0548b4b0334b249e15ae3b4914e
Fábio Santos <fabio.santos@fundacaoabc.org>
2020-09-12 13:51:56 -0300

    feat: configuração inicial do projeto, criação das páginas dashboard e repository
    Configurado ESLint, prettier, editor config, adicionado as páginas de dashboard e repository,
    configuração inicial do react-router-dom.

---
+++ src/routes/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
+import Repository from '../pages/Repository';
+
+// import { Container } from './styles';
+
+const Routes: React.FC = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={Dashboard} />
+      <Route path="/repository" component={Repository} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
